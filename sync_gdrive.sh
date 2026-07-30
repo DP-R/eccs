3e65544f1054c2c250cde1b5fa6bc0a62d0cf91b
dpr <purnendradatturi@gmail.com>
2026-07-31 00:09:44 +0530

    Update GoogleDrive sync target strictly to /home/dpr/GoogleDrive/gdrive/eccs

--- sync_gdrive.sh
+++ sync_gdrive.sh
@@ -2,14 +2,11 @@
 # ECCS Repository to GoogleDrive Sync Script
 
 REPO_DIR="/home/dpr/Downloads/eccs"
-GDRIVE_TARGET_1="/home/dpr/GoogleDrive/eccs"
-GDRIVE_TARGET_2="/home/dpr/GoogleDrive/gdrive/eccs"
+GDRIVE_TARGET="/home/dpr/GoogleDrive/gdrive/eccs"
 
-echo "[Sync] Syncing repository to GoogleDrive subfolders..."
+echo "[Sync] Syncing repository to GoogleDrive/gdrive/eccs..."
 
-mkdir -p "$GDRIVE_TARGET_1" "$GDRIVE_TARGET_2"
+mkdir -p "$GDRIVE_TARGET"
+rsync -av --delete --exclude='.git' --exclude='archive' --exclude='html' "$REPO_DIR/" "$GDRIVE_TARGET/"
 
-rsync -av --delete --exclude='.git' --exclude='archive' --exclude='html' "$REPO_DIR/" "$GDRIVE_TARGET_1/"
-rsync -av --delete --exclude='.git' --exclude='archive' --exclude='html' "$REPO_DIR/" "$GDRIVE_TARGET_2/"
-
-echo "[Sync] GoogleDrive subfolders sync complete."
+echo "[Sync] GoogleDrive/gdrive/eccs sync complete."
